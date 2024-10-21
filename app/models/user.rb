3370b2803d69013f2fb8d95d12f9735f6bc16a8a
Pakapol <meen260600@gmail.com>
2024-10-21 09:41:40 +0700

    Add user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates(:username, presence: true, uniqueness: true)
   validates(:email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP })
   validates(:password, length: { minimum: 8, maximum: 50 }, confirmation: true)
+  validates(:password_confirmation, presence: true)
 end
